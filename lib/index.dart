2b379644a145051c00960222836efce0256d0115
crypted420 <shedrack_samaila@yahoo.com>
2023-02-28 13:49:28 +0100

    ready for build

--- lib/index.dart
+++ lib/index.dart
@@ -14,11 +14,12 @@ class _CalulatorState extends State<Calculator> {
     final containerHeight = MediaQuery.of(context).size.height;
     final containerWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: darkmode ? Colors.black : Colors.white,
       body: Column(
         children: [
           Container(
               height: containerHeight / 2.5,
-              color: Colors.white,
+              color: darkmode ? Colors.black : Colors.white,
               child: resultDisplay(containerWidth, containerHeight)),
           Expanded(child: keyPads(containerWidth, containerHeight))
         ],
@@ -26,10 +27,12 @@ class _CalulatorState extends State<Calculator> {
     );
   }
 
+  bool darkmode = true;
+
   final keyPadsValue = [
     ['C', '^', '%', '÷'],
-    ['7', '8', '9', 'x'],
-    ['4', '5', '6', '-'],
+    ['7', '8', '9', '×'],
+    ['4', '5', '6', '−'],
     ['1', '2', '3', '+'],
     ['R', '0', '.', '=']
   ];
@@ -42,9 +45,10 @@ class _CalulatorState extends State<Calculator> {
         width: double.infinity,
         height: double.infinity,
         padding: const EdgeInsets.only(top: 20, bottom: 20),
-        decoration: const BoxDecoration(
-          color: Color.fromRGBO(249, 249, 249, 1),
-          borderRadius: BorderRadius.only(
+        decoration: BoxDecoration(
+          color:
+              darkmode ? Colors.black : const Color.fromRGBO(249, 249, 249, 1),
+          borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(40), topRight: Radius.circular(40)),
         ),
         child: Column(children: [
@@ -68,8 +72,15 @@ class _CalulatorState extends State<Calculator> {
       Expression exp = p.parse(expression);
       ContextModel cm = ContextModel();
       double result = exp.evaluate(EvaluationType.REAL, cm);
-      return result;
+
+      bool checkResult = result % 1 == 0 ? true : false;
+      if (checkResult) {
+        return result.round();
+      } else {
+        return result;
+      }
     } catch (e) {
+      log(e.toString());
       return 'syntax error';
     }
   }
@@ -83,20 +94,27 @@ class _CalulatorState extends State<Calculator> {
       inputs = '';
       expression = '';
       previousCal = '';
-    } else if (inputs.length > 20) {
+    } else if (inputs.length > 15) {
       inputs = inputs;
     } else if (keys[index] == '+' ||
-        keys[index] == '-' ||
+        keys[index] == '−' ||
         keys[index] == '÷' ||
         keys[index] == '^' ||
-        keys[index] == 'x' ||
+        keys[index] == '×' ||
         keys[index] == '%') {
-      inputs += ' ${keys[index]} ';
-      expression += '${keys[index] == 'x' ? '*' : keys[index]}';
+      inputs += '${keys[index]}';
+      expression +=
+          '${keys[index] == '×' ? '*' : (keys[index] == '÷' ? '/' : (keys[index] == '−' ? '-' : keys[index]))}';
     } else if (keys[index] == '=') {
-      previousCal = expression;
-      inputs = evaluateExpression().toString();
-      expression = inputs;
+      if (inputs.isNotEmpty) {
+        previousCal = expression;
+        inputs = evaluateExpression().toString();
+        expression = inputs;
+      } else {
+        previousCal = '';
+        inputs = '';
+        expression = '';
+      }
     } else {
       inputs += keys[index];
       expression += keys[index];
@@ -118,24 +136,41 @@ class _CalulatorState extends State<Calculator> {
                 BoxShadow(
                   color: Colors.grey.withOpacity(0.2),
                   spreadRadius: 0.1,
-                  blurRadius: 2,
-                  offset: const Offset(0, 1),
+                  blurRadius: darkmode ? 1 : 2,
+                  offset: darkmode ? const Offset(0, 0) : const Offset(0, 2),
                 )
               ],
-              color: Colors.grey[100],
+              color: darkmode
+                  ? const Color.fromRGBO(0, 0, 0, .5)
+                  : Colors.grey[100],
               borderRadius: BorderRadius.circular(15),
             ),
             child: Row(
               children: [
-                const Expanded(
-                  child: Icon(Icons.light_mode_outlined,
-                      color: Colors.black, size: 14),
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        darkmode = !darkmode;
+                      });
+                    },
+                    child: Icon(Icons.light_mode_outlined,
+                        color: darkmode ? Colors.grey[800] : Colors.black,
+                        size: darkmode ? 14 : 18),
+                  ),
                 ),
                 Expanded(
-                  child: Icon(
-                    Icons.dark_mode_outlined,
-                    color: Colors.grey[500],
-                    size: 14,
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        darkmode = true;
+                      });
+                    },
+                    child: Icon(
+                      Icons.dark_mode_outlined,
+                      color: darkmode ? Colors.grey[300] : Colors.grey[500],
+                      size: darkmode ? 18 : 14,
+                    ),
                   ),
                 )
               ],
@@ -146,31 +181,44 @@ class _CalulatorState extends State<Calculator> {
         // Inputs view
         Expanded(
           child: Container(
-              alignment: const Alignment(1.0, 1.0),
-              padding: const EdgeInsets.only(bottom: 15, right: 15, left: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    previousCal,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontFamily: 'OpenSans',
-                      fontSize: 25 - (inputs.length / 10).clamp(0, 3) * 5,
-                      fontWeight: FontWeight.w400,
+            alignment: const Alignment(1.0, 1.0),
+            padding: const EdgeInsets.only(bottom: 15, right: 15, left: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      previousCal,
+                      style: const TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'OpenSans',
+                          // fontSize: 25 - (inputs.length / 10).clamp(0, 3) * 5,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          textBaseline: TextBaseline.alphabetic),
                     ),
                   ),
-                  Text(
-                    inputs,
-                    style: TextStyle(
-                      fontFamily: 'OpenSans',
-                      fontSize: 40 - (inputs.length / 10).clamp(0, 3) * 5,
-                      fontWeight: FontWeight.w400,
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      inputs,
+                      style: TextStyle(
+                          fontFamily: 'OpenSans',
+                          // fontSize: 40 - (inputs.length / 10).clamp(0, 3) * 5,
+                          fontSize: inputs.length > 15 ? 40 : 50,
+                          fontWeight: FontWeight.w400,
+                          color: darkmode ? Colors.white : Colors.black),
                     ),
                   ),
-                ],
-              )),
+                ),
+              ],
+            ),
+          ),
         )
       ]),
     );
@@ -179,16 +227,17 @@ class _CalulatorState extends State<Calculator> {
   Expanded keyPadsContainer(final arrIndex, keys, containerWidth) {
     return Expanded(
       child: Wrap(
-        // spacing: 10,
         runSpacing: 10,
         children: List.generate(
           keys.length,
           (index) {
             Color textColor = arrIndex == 0
                 ? (index != 3
-                    ? const Color.fromARGB(179, 76, 175, 79)
-                    : Colors.red)
-                : (index != 3 ? Colors.black : Colors.red);
+                    ? const Color.fromRGBO(0, 192, 0, 1)
+                    : const Color.fromRGBO(255, 32, 0, 1))
+                : (index != 3
+                    ? (darkmode ? Colors.white : Colors.black)
+                    : const Color.fromRGBO(255, 32, 0, 1));
             return SizedBox(
               width: containerWidth / 4,
               height: 100,
@@ -216,15 +265,19 @@ class _CalulatorState extends State<Calculator> {
                     child: Center(
                         child: keys[index] == 'C'
                             ? const Icon(Icons.backspace_outlined,
-                                size: 25, color: Colors.black)
+                                size: 35, color: Color.fromRGBO(0, 192, 0, 1))
                             : (keys[index] == 'R'
-                                ? const Icon(Icons.refresh_outlined)
+                                ? Icon(
+                                    Icons.refresh_outlined,
+                                    color:
+                                        darkmode ? Colors.white : Colors.black,
+                                  )
                                 : Text(
                                     keys[index],
                                     style: TextStyle(
                                       fontFamily: 'OpenSans',
-                                      fontSize: 28,
-                                      fontWeight: FontWeight.w700,
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.w400,
                                       color: textColor,
                                     ),
                                   ))),
